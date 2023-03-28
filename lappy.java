9bfa452daeec6b7b40f3ea039b5a78c09e3b034f
Shailesh Sandeep Patil <124047221+parfil@users.noreply.github.com>
2023-03-28 11:35:36 +0530

    Add files via upload

---
+++ lappy.java
@@ -0,0 +1,15 @@
+class lappy
+{
+     public static void main(String args[]){
+	 
+	 
+	          asus as=()->{System.out.println("disply from Asus");};
+			  as.display();
+	}
+}
+@FunctionalInterface
+interface asus{
+     void display();
+}	 
+	 
+			  
